11e7b5cfe9ef3d29d9f1160424edc87e2359b044
Aakil Fernandes <aakilfernandes@gmail.com>
2020-01-21 13:09:40 -0500

    clean node

--- node/Bellflower.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Bellflower = /** @class */ (function () {
-    function Bellflower(provider) {
-        this.provider = provider;
-    }
-    return Bellflower;
-}());
-exports.Bellflower = Bellflower;
