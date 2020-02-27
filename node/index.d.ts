47f32ebbcfb53edeef1994acca2d6318576ffab2
Aakil Fernandes <aakilfernandes@gmail.com>
2020-02-27 12:17:23 -0500

    declarations

---
+++ node/index.d.ts
@@ -0,0 +1,17 @@
+import ethers from 'ethers';
+import { Snowdrop } from 'pollenium-snowdrop';
+import { Bytes32 } from 'pollenium-buttercup';
+export interface Block {
+    number: number;
+    hash: Bytes32;
+    timestamp: number;
+}
+export declare class Bellflower {
+    readonly provider: ethers.providers.Provider;
+    private latestBlock;
+    private latestBlockPrimrose;
+    readonly blockSnowdrop: Snowdrop<Block>;
+    constructor(provider: ethers.providers.Provider);
+    private linkProviderOnBlock;
+    private fetchLatestBlock;
+}
